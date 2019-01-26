e094cc55f54e9fffd17b5a08cd90745337fcdfd6
Daniel Schulz <faulolio@gmail.com>
2019-01-26 15:47:59 +0100

    IInteractable, torches

---
+++ FindingHome/Assets/Scripts/Torch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour, IInteractable {
+    private bool IsLit = false;
+    public void Interact(GameObject actor) {
+        IsLit = !IsLit;
+    }
+
+    private GameObject Sparks {
+        get {
+            return transform.Find("Sparks").gameObject;
+        }
+    }
+    private GameObject Ember {
+        get {
+            return transform.Find("Ember").gameObject;
+        }
+    }
+
+    private ParticleSystem.EmissionModule SparksEmission {
+        get {
+            return Sparks.GetComponent<ParticleSystem>().emission;
+        }
+    }
+    private Light TorchLight {
+        get {
+            return GetComponentInChildren<Light>();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var emission = SparksEmission;
+
+        emission.enabled = IsLit;
+        TorchLight.enabled = IsLit;
+    }
+}
