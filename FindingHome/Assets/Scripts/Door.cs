4e49a7c5e41d70622a12d4709d168700588b33d9
alex <a_dombert@web.de>
2019-01-26 12:48:44 +0100

    Level1

--- FindingHome/Assets/Scripts/Door.cs
+++ FindingHome/Assets/Scripts/Door.cs
@@ -8,7 +8,7 @@ public class Door : MonoBehaviour, IMechanism
 
     Vector3 originalPosition;
 
-    float moveAmount = 2f;
+    public float moveAmount = 2f;
 
     // Start is called before the first frame update
     void Start()
