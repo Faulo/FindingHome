61f1dba04532a1524518d8287ffd10cdb88a3fd9
Daniele Martiens <d.martines@gmx.de>
2019-01-26 12:58:34 +0100

    breakable wall

---
+++ FindingHome/Assets/Scripts/WallBreak.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBreak : MonoBehaviour
+{
+    private Rigidbody[] _wallBody;
+
+    private void Awake()
+    {
+        _wallBody = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody body in _wallBody)
+        {
+            body.isKinematic = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fox")
+        {
+            foreach (Rigidbody body in _wallBody)
+            {
+                body.isKinematic = false;
+            }
+        }
+    }
+
+}
