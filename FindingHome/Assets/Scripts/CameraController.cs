14a58c56c738399d2828fdf8778cbbd942384ce1
alex <a_dombert@web.de>
2019-01-26 01:11:36 +0100

    few changes on cameracontroller script

--- FindingHome/Assets/Scripts/CameraController.cs
+++ FindingHome/Assets/Scripts/CameraController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform target;
+
+    public float smoothTime = 6f;
+    public float yDistanceToTarget = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y + yDistanceToTarget, target.position.z);
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, smoothTime * Time.deltaTime);
     }
 }
