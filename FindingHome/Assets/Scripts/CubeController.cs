167e7b46cbea3e5178c7c3cd456494a51447ca83
Daniel Schulz <faulolio@gmail.com>
2019-01-27 11:51:13 +0100

    fixed multi-object switch bug

--- FindingHome/Assets/Scripts/CubeController.cs
+++ FindingHome/Assets/Scripts/CubeController.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CubeController : MonoBehaviour
-{
+public class CubeController : MonoBehaviour, IActor {
     public static float PlayerDistance {
         get {
             var list = FindObjectsOfType<CubeController>();
