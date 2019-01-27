a70b8d374490d9af350ca545daf17261442b47e5
alex <a_dombert@web.de>
2019-01-27 16:39:32 +0100

    final scene

---
+++ FindingHome/Assets/Scripts/EndSceneHack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndSceneHack : MonoBehaviour
+{
+    public GameObject Final;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowCredits());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ShowCredits()
+    {
+        yield return new WaitForSeconds(8f);
+        Final.SetActive(false);
+    }
+}
